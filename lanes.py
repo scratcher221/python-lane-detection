5d66c5ad13e161435bd6421997c76b3115739da2
scratcher221 <mitterlehner.david@gmail.com>
2019-11-22 19:50:07 +0100

    third commit.

--- lanes.py
+++ lanes.py
@@ -1,5 +1,7 @@
 def commence_image_regognition():
+    image = open("test_image.jpg", "r")
     print(F"Commencing image recoginition ... ")
+    image.close()
 
 if __name__ == "__main__":
     commence_image_regognition()

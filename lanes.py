c6feb6a0fc33bb162357c436af317b79958c85fd
scratcher221 <mitterlehner.david@gmail.com>
2019-11-23 18:47:19 +0100

    Started implementing lane detection in image like in the tutorial.

--- lanes.py
+++ lanes.py
@@ -1,7 +1,24 @@
-def commence_image_regognition():
-    image = open("test_image.jpg", "r")
-    print(F"Commencing image recoginition .... ")
-    image.close()
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    commence_image_regognition()
+def canny(image):
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+        [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
+image = cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+canny = canny(lane_image)
+cv2.imshow('edges', region_of_interest(canny))
+cv2.waitKey(0)
